0473707dc4d1cca3a906754a604f3221ee6c0d03
Daboy <catalunesroy@gmail.com>
2023-11-13 13:59:00 +0800

    user-model

---
+++ user/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import UserManager
+
+# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=50, unique=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    objects = UserManager()
+
+    date_joined = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def user_email(self):
+        return self.email
